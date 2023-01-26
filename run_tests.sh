90c8a72c7e752cde9e53513e4b7afa0506c058f7
Thomas <thomash1996@live.com>
2023-01-25 18:59:08 -0600

    updated name for clarity

--- run_tests.sh
+++ run_tests.sh
@@ -21,5 +21,5 @@ do
     python mocap_pose_to_csv.py -i $rb -o $rb
     python control_inputs_to_csv.py -i $rb -o $rb
     python gnss_to_csv.py -i $rb -o $rb
-    python mult_thread_camera_to_png.py -i $rb -o $rb
+    python camera_to_png_threadpool.py -i $rb -o $rb
 done
